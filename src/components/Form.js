f9776fc8ae2cc099e2b4fd5b6ba8a6d5963d3658
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-11-09 17:47:41 -0700

    edit styling and add collection button

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,13 +12,14 @@ const Form = (props) => {
     setSearch('')
   }
 
+
   return (
-    <div>
+    <div className='right-container'>
       <form className='keyword-form'>
-        <input type='text' placeholder='type here' className='input' value={search} onChange={event => setSearch(event.target.value)} required/>
+        <input type='text' placeholder='SEARCH SOMETHING!!' className='input' value={search} onChange={event => setSearch(event.target.value)} required/>
         <button type='submit' className='submit' onClick={submitKeyword}>GO</button>
       </form>
-
+      <button className='collection'>COLLECTION</button>
     </div>
   )
 }
